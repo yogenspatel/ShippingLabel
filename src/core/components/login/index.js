004da29433d11cb216bca7da07d85f41eab3513f
Yogen Patel <yogen.patel@nbcuni.com>
2019-04-27 12:24:22 -0400

    Minor style tweaks for login component

--- src/core/components/login/index.js
+++ src/core/components/login/index.js
@@ -3,30 +3,50 @@ import LoginHOC from './loginHOC';
 import ShippingLabelMaker from '../../../features/shipping-label-maker';
 import { renderError } from '../../../utilities/utils';
 
-const Login = ({ username, password, handleOnChange, handleOnSubmit, isLoggedIn, error }) => {
-  return !isLoggedIn ? (
-    <div className="card">
-      <div className="card-header text-center h3">Login (Username = test | Password = test)</div>
-      <form onSubmit={handleOnSubmit}>
-        {error && renderError("Incorrect Username/Password")}
-        <div className="input-group input-group-lg mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text">Username</span>
+class Login extends React.Component {
+  componentDidMount() {
+    this.userNameInput.focus();
+  }
+  render() {
+    const { username, password, handleOnChange, handleOnSubmit, isLoggedIn, error } = this.props;
+    return !isLoggedIn ? (
+      <div className="card">
+        <div className="card-header text-center h3">Login (Username = test | Password = test)</div>
+        <form onSubmit={handleOnSubmit}>
+          {error && renderError("Incorrect Username/Password")}
+          <div className="input-group input-group-lg mb-3 ml-5 mt-5 pr-5">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Username</span>
+            </div>
+            <input
+              type="text"
+              autoComplete="new-username"
+              className="form-control"
+              name="username"
+              value={username}
+              onChange={handleOnChange}
+              ref={(input) => { this.userNameInput = input; }}
+            />
           </div>
-          <input type="text" className="form-control" name="username" value={username} onChange={handleOnChange} />
-        </div>
-        <div className="input-group input-group-lg mb-3">
-          <div className="input-group-prepend">
-            <span className="input-group-text">Password</span>
+          <div className="input-group input-group-lg mb-3 ml-5 pr-5">
+            <div className="input-group-prepend">
+              <span className="input-group-text">Password</span>
+            </div>
+            <input
+              type="password"
+              autoComplete="new-password"
+              className="form-control"
+              name="password" value={password}
+              onChange={handleOnChange}
+            />
           </div>
-          <input type="password" name="password" value={password} onChange={handleOnChange} />
-        </div>
-        <div className="text-center mb-3">
-          <input className="btn btn-primary" type="submit" value="Login" />
-        </div>
-      </form>
-    </div>
-  ) : <ShippingLabelMaker />; 
+          <div className="text-center mb-3">
+            <input className="btn btn-primary" type="submit" value="Login" />
+          </div>
+        </form>
+      </div>
+    ) : <ShippingLabelMaker />; 
+  }
 } 
 
 export default (LoginHOC(Login));
